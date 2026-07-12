fefcbc7e266324fdc065ffc5b3c81cc2b439b7c1
shaharsol <shahar@gnift.com>
2026-07-12 21:30:39 +0300

    if else

---
+++ 2026-07-12/16-conditions/app.js
@@ -0,0 +1,9 @@
+// ask the clinet for their age
+age = +prompt('enter your age')
+
+// determine if the client is a child or adult
+isAdult = age > 18 // 13 > 18 = false, 21 > 18 = true, 18 > 18 = false
+
+if(isAdult) alert('welcome!')
+else alert('go away child, come back tomorrow')     
+
