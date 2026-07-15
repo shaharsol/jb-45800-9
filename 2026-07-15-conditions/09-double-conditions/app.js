f2886e1ad5461242bed36d03315421fbf4e5e479
shaharsol <shahar@gnift.com>
2026-07-15 21:02:12 +0300

    alota

--- 2026-07-15-conditions/09-double-conditions/app.js
+++ 2026-07-15-conditions/09-double-conditions/app.js
@@ -2,7 +2,8 @@ grade = +prompt('enter a grade')
 
 if (grade > 100 || grade < 0) {
     alert('invalid')
-} else if (grade > 60 || grade == 60) {
+// } else if (grade > 60 || grade == 60) {
+} else if (grade >= 60) {
     alert('pass')
 } else {
     alert('fail')
