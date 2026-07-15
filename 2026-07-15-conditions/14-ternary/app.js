754027232e577b81a1787f6183ba25e51be8c066
shaharsol <shahar@gnift.com>
2026-07-15 21:00:35 +0300

    ternary

---
+++ 2026-07-15-conditions/14-ternary/app.js
@@ -0,0 +1,19 @@
+grade = +prompt('enter your grade')
+
+// if (grade > 60) {
+//     alert('pass')
+// } else {
+//     alert('fail')
+// }
+
+
+// expressions are built from oprands and operators
+// 4 + 5 = 9
+// 4 is an operand
+// 5 is an operand
+// + is an opertaor
+// most expressions are built from 2 operands and one operator
+
+
+// ternary operator is a special operator that has 3 legs and "two" operands
+alert(grade > 60 && grade <= 100 ? 'pass' : 'fail')
---
+++ 2026-07-15-conditions/15-class-exercise-6/app.js
@@ -0,0 +1,4 @@
+number1 = +prompt('enter a number')
+number2 = +prompt('enter antother number')
+
+alert(number1 > number2 ? `${number1} is bigger` : `${number2} is bigger`)
