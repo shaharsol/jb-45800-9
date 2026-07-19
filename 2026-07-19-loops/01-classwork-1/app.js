dd9f6474f1f49996175e7bf2d21710426026e8d4
shaharsol <shahar@gnift.com>
2026-07-19 17:26:15 +0300

    ifs

---
+++ 2026-07-19-loops/01-classwork-1/app.js
@@ -0,0 +1,28 @@
+number1 = +prompt('enter a number')
+number2 = +prompt('enter a 2nd number')
+number3 = +prompt('enter a 3rd number')
+
+// method 1:
+// if (number1 > number2) {
+//     if(number1 > number3) {
+//         alert(`${number1} is the max`)
+//     } else {
+//         alert(`${number3} is the max`)
+//     }
+// } else {
+//    if(number2 > number3) {
+//         alert(`${number2} is the max`)
+//     } else {
+//         alert(`${number3} is the max`)
+//     } 
+// }
+
+// method2:
+if (number1 > number2 && number1 > number3) {
+    alert(`${number1} is max`)
+} else if (number2 > number1 && number2 > number3) {
+    alert(`${number2} is max`)
+} else {
+    alert(`${number3} is max`)
+}
+
