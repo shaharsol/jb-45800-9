b2568d757043b734bccb27085b0d8f16564acaba
shaharsol <shahar@gnift.com>
2026-07-19 19:41:32 +0300

    finished while loop

---
+++ 2026-07-19-loops/09-classwork-7/app.js
@@ -0,0 +1,9 @@
+while (dayNumber <= 8) {
+dayNumber = 1
+sum = 0
+
+    sum = sum + dayNumber + 1 // the +1 is the SHAMASH
+    dayNumber = dayNumber + 1
+}
+
+console.log(`total number of candles is ${sum}`)
