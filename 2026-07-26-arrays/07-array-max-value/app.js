f38fb5f85c1d7c262edfa1399a4024b4fc950efe
shaharsol <shahar@gnift.com>
2026-07-26 19:12:36 +0300

    array max

--- 2026-07-26-arrays/07-array-max-value/app.js
+++ 2026-07-26-arrays/07-array-max-value/app.js
@@ -1,1 +1,12 @@
-grades = [90, 88, 100, 80, 40, 98, 66, 79, 45]
+grades = [90, 88, 80, 40, 98, 66, 79, 45]
+
+// find maximum grade
+max = 0
+maxOffset = -1
+for (offset = 0; offset < grades.length; offset++) {
+    if (grades[offset] > max) {
+        max = grades[offset]
+        maxOffset = offset
+    }
+}
+console.log(`maximum grade is ${max}, in offset ${maxOffset}`)
