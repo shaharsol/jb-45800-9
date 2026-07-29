f4344623a3672ba6e55a4feabbd63c1ecaa0dda1
shaharsol <shahar@gnift.com>
2026-07-29 18:17:57 +0300

    alice

--- 2026-07-29/01-homework-1-2/app.js
+++ 2026-07-29/01-homework-1-2/app.js
@@ -1,6 +1,8 @@
 songs = []
 
 for(song = 0; song < 3; song++) {
+    // songName = prompt("enter a song name")
+    // songs.push(songName)
     songs.push(prompt("enter a song name"))
 }
 console.log(songs)
---
+++ 2026-07-29/02-array-of-objects-exercise/app.js
@@ -0,0 +1,20 @@
+users = [
+    {
+        id: 1,
+        name: 'Alice',
+        following: [25, 46, 106]
+    },
+    {
+        id: 2,
+        name: 'Bob',
+        following: [78, 46, 25]
+    },
+    {
+        id: 3,
+        name: 'Diana',
+        following: [25, 106, 79]
+    }
+]
+
+// prompt the user for a user id
+// calculate how many followers this user id have
