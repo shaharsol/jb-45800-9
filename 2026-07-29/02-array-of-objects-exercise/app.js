bc444e2aefc8216e605c9f7aeab5e01c6ec94aaa
shaharsol <shahar@gnift.com>
2026-07-29 18:29:28 +0300

    users and followers

--- 2026-07-29/02-array-of-objects-exercise/app.js
+++ 2026-07-29/02-array-of-objects-exercise/app.js
@@ -18,3 +18,17 @@ users = [
 
 // prompt the user for a user id
 // calculate how many followers this user id have
+
+userId = +prompt("enter the user id")
+numberOfFollowers = 0
+
+for (user of users) {
+    for(followingId of user.following) {
+        if(userId == followingId) {
+            numberOfFollowers++
+            break
+        }
+    }    
+}
+
+console.log(`user id ${userId} has ${numberOfFollowers} followers`)
