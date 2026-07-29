8626dbc15a7ba286a0c55ed21934ef7e15fca36b
shaharsol <shahar@gnift.com>
2026-07-29 20:11:54 +0300

    intro to funcs

--- 2026-07-29/06-dont-repeat-yourself/app.js
+++ 2026-07-29/06-dont-repeat-yourself/app.js
@@ -1,1 +1,14 @@
-const firstName 
+const firstName = prompt('enter your first name')
+const lastName = prompt('enter your last name')
+
+console.log(`your full name is ${firstName} ${lastName}`)
+
+const user = {
+    name: {
+        first: firstName,
+        last: lastName
+    }
+}
+
+console.log(`your full name is ${user.name.first} ${user.name.last}`)
+
