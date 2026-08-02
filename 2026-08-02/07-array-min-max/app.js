b3a00d5229230b5460c7191ab02a8d19fd386114
shaharsol <shahar@gnift.com>
2026-08-02 19:01:22 +0300

    alota

--- 2026-08-02/07-array-min-max/app.js
+++ 2026-08-02/07-array-min-max/app.js
@@ -1,15 +1,20 @@
 function getArrayMinMax(array) {
-    let max = -Infinity
-    let min = Infinity
+    let max = -Infinity // in grades in would be 0
+    let min = Infinity // in grades it would be 100
     
     for (const number of array) {
         if (number > max) max = number
         if (number < min) min = number
     }
 
+    // return {
+    //     max: max,
+    //     min: min
+    // }
+
     return {
-        max: max,
-        min: min
+        min,
+        max
     }
 }
 
