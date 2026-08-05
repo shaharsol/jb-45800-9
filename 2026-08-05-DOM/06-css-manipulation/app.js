5f7dcd60d47517f408408912c320aafd2faec614
shaharsol <shahar@gnift.com>
2026-08-05 19:54:03 +0300

    manipulating css

---
+++ 2026-08-05-DOM/06-css-manipulation/app.js
@@ -0,0 +1,6 @@
+function manipulate() {
+    const mySpan = document.getElementById('my-span')
+    // mySpan.style = "color: red;"
+    mySpan.className = "red"
+    mySpan.classList.add("italic")
+}
