510507e5916bad9bd990a39c8b52d6cbf86605c2
shaharsol <shahar@gnift.com>
2026-08-09 18:04:53 +0300

    alota

---
+++ 2026-08-09-preps-for-mission-1/05-image-url-input/app.js
@@ -0,0 +1,27 @@
+function addUser() {
+    // dont submit the form
+    event.preventDefault()
+    // instead run my code
+    const firstName = document.getElementById('first-name').value
+    const lastName = document.getElementById('last-name').value
+    const email = document.getElementById('email').value
+    const cv = document.getElementById('cv').value
+    const category = document.getElementById('category').value
+    const color = document.getElementById('color').value
+
+
+    const tableBody = document.getElementById('user-table-body')
+
+    tableBody.innerHTML += `
+        <tr style="background-color: ${color};">
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${email}</td>
+            <td>${category}</td>
+            <td>${cv}</td>
+        </tr>
+    `
+
+    document.getElementById('user-form').reset()
+    
+}
