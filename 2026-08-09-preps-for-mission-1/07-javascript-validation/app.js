8847fde4fee5f4874797f138c7152627fa341b97
shaharsol <shahar@gnift.com>
2026-08-09 18:51:15 +0300

    js validation

---
+++ 2026-08-09-preps-for-mission-1/07-javascript-validation/app.js
@@ -0,0 +1,37 @@
+function isCVcontainsEducation(cv) {
+    return cv.includes('education')
+}
+
+function addUser() {
+    // dont submit the form
+    event.preventDefault()
+    // instead run my code
+    const firstName = document.getElementById('first-name').value
+    const lastName = document.getElementById('last-name').value
+    const email = document.getElementById('email').value
+    const cv = document.getElementById('cv').value
+    const category = document.getElementById('category').value
+    const color = document.getElementById('color').value
+    const pic = document.getElementById('pic').value
+
+    if (!isCVcontainsEducation(cv)) {
+        alert('CV must contain an Education section')
+        return
+    }
+
+    const tableBody = document.getElementById('user-table-body')
+
+    tableBody.innerHTML += `
+        <tr style="background-color: ${color};">
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${email}</td>
+            <td>${category}</td>
+            <td>${cv}</td>
+            <td><img src="${pic}" /></td>
+        </tr>
+    `
+
+    document.getElementById('user-form').reset()
+    
+}
