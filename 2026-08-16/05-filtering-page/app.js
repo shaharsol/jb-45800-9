01d205ef2a5abdaf48dc692532ed419390b87385
shaharsol <shahar@gnift.com>
2026-08-16 17:39:53 +0300

    filter by category example

---
+++ 2026-08-16/05-filtering-page/app.js
@@ -0,0 +1,164 @@
+// this is the convention to name a variable which stands as a logical constant
+const USERS_LOCAL_STORAGE_KEY = 'users'
+
+// read users from the localStorage
+function getUsers() {
+    // const users = localStorage.getItem(USERS_LOCAL_STORAGE_KEY)
+    // if (users) return JSON.parse(users)
+    // return []    
+    return JSON.parse(localStorage.getItem(USERS_LOCAL_STORAGE_KEY) || "[]")    
+}
+
+function saveUsers(users) {
+    localStorage.setItem(USERS_LOCAL_STORAGE_KEY, JSON.stringify(users))
+    syncToDom()
+}
+
+// will read from localstorage (SSOT) and sync to dom
+function syncToDom() {
+    const users = getUsers()
+    
+    const tableBody = document.getElementById('user-table-body')
+    let newRows = ''
+    let rowNumber = 0;
+
+    for (user of users) {
+
+        newRows += `
+            <tr id="tr-${rowNumber}" style="background-color: ${user.color};">
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.email}</td>
+                <td>${user.category}</td>
+                <td>${user.cv}</td>
+                <td><img src="${user.pic}" /></td>
+                <td>
+                    <button class="delete-button" onclick="deleteRow(${rowNumber})">Delete</button>
+                    <button onclick="populateUpdateForm(${rowNumber})">Update</button>
+                </td>
+            </tr>
+        `     
+        
+        rowNumber++
+    }
+
+    tableBody.innerHTML = newRows
+
+}
+
+// principals:
+// 1. each data operation begins with getUsers
+// 2. each data modification should be done on the returned json (SSOT) and then sent back to saveUsers
+
+
+function isCVcontainsEducation(cv) {
+    return cv.includes('education')
+}
+
+function isFirstLetterCapital(str) {
+    const firstLetter = str[0] // str.substr(0,1)
+    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(firstLetter)
+}
+
+function deleteRow(rowNumber) {
+    if(confirm("are you sure u want to delete?")){
+        const users = getUsers()
+        users.splice(rowNumber, 1)    
+        saveUsers(users)
+    }
+}
+
+function populateUpdateForm(rowNumber) {
+    const users = getUsers()
+    document.getElementById('first-name').value = users[rowNumber].firstName
+    document.getElementById('last-name').value = users[rowNumber].lastName
+    document.getElementById('email').value = users[rowNumber].email
+}
+
+function resetForm() {
+    document.getElementById('user-form').reset()
+}
+
+function addUser() {
+    // dont submit the form
+    event.preventDefault()
+    // instead run my code
+    const firstName = document.getElementById('first-name').value
+    const lastName = document.getElementById('last-name').value
+    const email = document.getElementById('email').value
+    const cv = document.getElementById('cv').value
+    const category = document.getElementById('category').value
+    const color = document.getElementById('color').value
+    const pic = document.getElementById('pic').value
+
+    if(firstName === 'Yosef') {
+        if(!lastName) alert('must add a last name for Yosef')
+    }
+
+
+    if(!isFirstLetterCapital(firstName)) {
+        alert('First name must start with a capital letter')
+        return
+    }
+    if (!isCVcontainsEducation(cv)) {
+        alert('CV must contain an Education section')
+        return
+    }
+
+    const users = getUsers()
+    users.push({
+        firstName,
+        lastName,
+        email,
+        cv,
+        category,
+        color,
+        pic
+    })
+    saveUsers(users)
+
+    resetForm()
+    
+}
+
+function firstNameChanged() {
+    document.getElementById('last-name').required = document.getElementById('first-name').value === 'Yosef'
+}
+
+function filterByCategory() {
+    const users = getUsers()
+    
+    const tableBody = document.getElementById('user-table-body')
+    let newRows = ''
+    let rowNumber = 0;
+
+    for (user of users) {
+
+        if(user.category === document.getElementById('category-filter').value) {
+            newRows += `
+                <tr id="tr-${rowNumber}" style="background-color: ${user.color};">
+                    <td>${user.firstName}</td>
+                    <td>${user.lastName}</td>
+                    <td>${user.email}</td>
+                    <td>${user.category}</td>
+                    <td>${user.cv}</td>
+                    <td><img src="${user.pic}" /></td>
+                    <td>
+                        <button class="delete-button" onclick="deleteRow(${rowNumber})">Delete</button>
+                        <button onclick="populateUpdateForm(${rowNumber})">Update</button>
+                    </td>
+                </tr>
+            `     
+            
+            rowNumber++
+
+        }
+
+    }
+
+    tableBody.innerHTML = newRows
+        
+}
+
+syncToDom()
+
