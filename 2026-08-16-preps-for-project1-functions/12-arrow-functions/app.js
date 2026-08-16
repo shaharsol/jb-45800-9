334f98fe4949fd40712230d9fc5681963287b2cb
shaharsol <shahar@gnift.com>
2026-08-16 19:43:25 +0300

    arrow functions are the norm

---
+++ 2026-08-16-preps-for-project1-functions/12-arrow-functions/app.js
@@ -0,0 +1,61 @@
+const students = [{
+    firstName: 'sasson',
+    lastName: 'Cohen',
+    country: 'USA',
+    age: 40,
+    gender: 'male'
+}, {
+    firstName: 'lavie',
+    lastName: 'hefetz',
+    country: 'France',
+    age: 20,
+    gender: 'male'
+
+}, {
+    firstName: 'Ruth',
+    lastName: 'Moav',
+    country: 'Italy',
+    age: 30,
+    gender: 'female'
+
+}, {
+    firstName: 'Judith',
+    lastName: 'Lauren',
+    country: 'UK',
+    age: 60,
+    gender: 'female'
+
+}, {
+    firstName: 'Pierre',
+    lastName: 'Dechamp',
+    country: 'France',
+    age: 38,
+    gender: 'male'
+
+}, {
+    firstName: 'Edith',
+    lastName: 'Piaf',
+    country: 'France',
+    age: 70,
+    gender: 'female'
+
+}]
+
+
+function mapArray(array, callback) {
+    const result = []
+    for (const item of array) {
+        result.push(callback(item))
+    }
+    return result
+}
+
+
+// in 2015, javascript introduced the arrow functions
+// originally intended to serve as callback functions
+// but quickly became all over the place and for a good reason
+const mappedArray = mapArray(students, (student) => {
+    return {fullName: `${student.firstName} ${student.lastName}`}
+})
+
+console.log(mappedArray)
---
+++ 2026-08-16-preps-for-project1-functions/13-arrow-functions-strength/app.js
@@ -0,0 +1,12 @@
+// function sayHello() {
+//     console.log('hello')
+// }
+
+const sayHello = () => {
+    console.log('hello')
+}
+
+
+sayHello = 1
+console.log(sayHello)
+sayHello()
