37b51974fc2c4f42a7ff5ed1e2e82533c857bb46
shaharsol <shahar@gnift.com>
2026-08-19 18:09:39 +0300

    clock

--- 2026-08-19-async/04-clock/app.js
+++ 2026-08-19-async/04-clock/app.js
@@ -6,4 +6,6 @@ setInterval(() => {}, 10 * 60 * 1000)
 
 using this new information (new shit has come to light) draw a clock on the page
 
+const now = new Date() // <- this gives you a date object containing the current time
+
 */
