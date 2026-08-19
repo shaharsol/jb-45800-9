f93ed13eb7ce8e6fa4e47f4db025d50ed7c8e588
shaharsol <shahar@gnift.com>
2026-08-19 19:11:06 +0300

    hannukah

--- 2026-08-19-async/09-hanukkah/app.js
+++ 2026-08-19-async/09-hanukkah/app.js
@@ -1,8 +1,16 @@
 // const getCandlesPerDay = dayNumber => dayNumber + 1
 // console.log(`number of days on 4 day is ${getCandlesPerDay(4)}`)
 
+// use this server mock to calc the number of
+// candles used in Hannukah
+// dont print garbage. provide a single answer and console log
+// it when you are ready
 const howManyCandlesPerDay = (dayNumber, callback) => {
     setTimeout(() => {
         callback(dayNumber + 1)
     }, 1 * 1000)
 }
+
+
+howManyCandlesPerDay(1, answer => {
+})
