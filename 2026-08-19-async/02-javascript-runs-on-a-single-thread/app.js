94ead596961b0ef852c110a49210d522844400ac
shaharsol <shahar@gnift.com>
2026-08-19 17:56:06 +0300

    alota

---
+++ 2026-08-19-async/02-javascript-runs-on-a-single-thread/app.js
@@ -0,0 +1,10 @@
+const changeColor = () => {
+    document.getElementById('color-button').style.backgroundColor = document.getElementById('color-button').style.backgroundColor === 'red' ? 'blue' : 'red'
+}
+
+const getData = () => {
+    for (let i = 0 ; i < 300000; i++ ) {
+        console.log(i)
+    }
+    document.getElementById('data').innerHTML = 'here is the data from the server...'
+}
