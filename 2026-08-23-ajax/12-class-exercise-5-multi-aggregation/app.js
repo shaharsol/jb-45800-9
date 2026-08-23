dec6cd27e7bf9e636454134dc5bba597ffc4e7b6
shaharsol <shahar@gnift.com>
2026-08-23 20:43:13 +0300

    alota

--- 2026-08-23-ajax/12-class-exercise-5-multi-aggregation/app.js
+++ 2026-08-23-ajax/12-class-exercise-5-multi-aggregation/app.js
@@ -10,7 +10,38 @@ const getData = url => fetch(url).then(response => response.json());
 (async () => {
     try {
         const { users } = await getData('https://dummyjson.com/users')
-    
+        document.getElementById('state-counts').innerHTML = users.reduce((cumulative, { address: { state }}) => {
+            const clonedCumulativce = [...cumulative]
+            const currentState = clonedCumulativce.find(obj => obj.state === state)
+            if (currentState) {
+                currentState.count++
+            } else {
+                clonedCumulativce.push({
+                    state,
+                    count: 1
+                })
+            }
+            return clonedCumulativce
+        }, []).map(({state, count}) => `
+            <tr>
+                <td>${state}</td>
+                <td>${count}</td>
+            </tr>
+        `).join('')
+        
+
+/*
+[
+    {
+        state: 'Mississipi',
+        count: 9
+    },
+    {
+        state: 'Utah',
+        count: 6
+    }
+]
+*/
     } catch (e) {
         console.log(e)
     }
