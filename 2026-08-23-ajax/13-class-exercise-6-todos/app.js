67b3395522f390e629cd140b3b41cd93c35bf62c
shaharsol <shahar@gnift.com>
2026-08-23 20:54:03 +0300

    todos finished

--- 2026-08-23-ajax/13-class-exercise-6-todos/app.js
+++ 2026-08-23-ajax/13-class-exercise-6-todos/app.js
@@ -9,35 +9,38 @@ const getData = url => fetch(url).then(response => response.json());
 
 (async () => {
     try {
-        const { users } = await getData('https://dummyjson.com/users')
-        document.getElementById('state-counts').innerHTML = users.reduce((cumulative, { address: { state }}) => {
-            const clonedCumulativce = [...cumulative]
-            const currentState = clonedCumulativce.find(obj => obj.state === state)
-            if (currentState) {
-                currentState.count++
-            } else {
-                clonedCumulativce.push({
-                    state,
-                    count: 1
-                })
-            }
-            return clonedCumulativce
-        }, []).map(({state, count}) => `
-            <tr>
-                <td>${state}</td>
-                <td>${count}</td>
-            </tr>
-        `).join('')
+        const { todos } = await getData('https://dummyjson.com/todos')
+        document.getElementById('todos-counts').innerHTML = todos
+            .reduce((cumulative, { userId }) => {
+                const clonedCumulativce = [...cumulative]
+                const currentUserId = clonedCumulativce.find(obj => obj.userId === userId)
+                if (currentUserId) {
+                    currentUserId.count++
+                } else {
+                    clonedCumulativce.push({
+                        userId,
+                        count: 1
+                    })
+                }
+                return clonedCumulativce
+            }, [])
+            .map(({userId, count}) => `
+                <tr>
+                    <td>${userId}</td>
+                    <td>${count}</td>
+                </tr>
+            `)
+            .join('')
         
 
 /*
 [
     {
-        state: 'Mississipi',
+        userId: 167,
         count: 9
     },
     {
-        state: 'Utah',
+        userId: 45,
         count: 6
     }
 ]
